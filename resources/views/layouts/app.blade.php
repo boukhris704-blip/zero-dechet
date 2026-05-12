67f39c8a2801d47af06099a5803554a306fba50a
lamis oueslati <lamisoueslati26@gmail.com>
2026-05-12 10:50:40 +0100

    Lamis - Design Interface UI UX Bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -166,3 +166,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+<!-- Lamis Oueslati - Design UI UX Bootstrap Animations -->
